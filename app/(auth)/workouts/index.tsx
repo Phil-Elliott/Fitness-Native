c10d0e7041f238869d27992a749715e750fa3a4f
Phil <phillippelliott@gmail.com>
2023-11-01 13:32:24 +0800

    created workouts header

--- app/(auth)/workouts/index.tsx
+++ app/(auth)/workouts/index.tsx
@@ -1,16 +1,27 @@
-import { Link } from "expo-router";
 import React from "react";
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
+import Header from "../../../components/workouts/Header";
 
 const Workouts = () => {
   return (
-    <View>
-      <Text>Workouts</Text>
-      <Link href="/workouts/1">Workouts 1</Link>
-      <Link href="/workouts/2">Workouts 2</Link>
-      <Link href="/workouts/addWorkout">Workouts 3</Link>
+    <View className="h-full pt-10 px-4 dark:bg-gray-700 dark:text-white">
+      <Header />
     </View>
   );
 };
 
 export default Workouts;
+
+/*
+<View>
+      <Text>Workouts</Text>
+      <Link href="/workouts/1">Workouts 1</Link>
+      <Link href="/workouts/2">Workouts 2</Link>
+      <Link href="/workouts/addWorkout">Workouts 3</Link>
+    </View>
+
+  
+
+
+*/
