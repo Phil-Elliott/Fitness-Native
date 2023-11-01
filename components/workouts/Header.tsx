0115cc8ef5a7a155c821277fcbe95f3c7e28390e
Phil <phillippelliott@gmail.com>
2023-11-01 13:42:54 +0800

    finished workouts header basic design

--- components/workouts/Header.tsx
+++ components/workouts/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View className="flex-row justify-between items-center">
+      <Text className="text-xl font-bold">Workouts</Text>
+      <View className="flex-row gap-4">
+        <Icon name="search-outline" size={24} color="black" />
+        <Icon name="filter" size={24} color="black" />
+        <Icon name="calendar-outline" size={24} color="black" />
+        <Icon name="add-circle-outline" size={24} color="black" />
+      </View>
     </View>
   );
 };
