168c0ef7ba280c505c75f322b3abee918f4defd4
Phil <phillippelliott@gmail.com>
2023-11-04 10:13:58 +0800

    connected to server

--- app/(auth)/workouts/index.tsx
+++ app/(auth)/workouts/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { View } from "react-native";
 import Header from "../../../components/workouts/Header";
 import ScrollCalendar from "../../../components/workouts/ScrollCalendar";
+import Upcoming from "../../../components/workouts/Upcoming";
 
 const Workouts = () => {
   return (
     <View className="h-full pt-10 px-4 dark:bg-gray-700 dark:text-white">
       <Header />
-      <ScrollCalendar />
+      {/* <ScrollCalendar /> */}
+      <Upcoming />
     </View>
   );
 };
