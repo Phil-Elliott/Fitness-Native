1ad0dc6dd16bd1ba684d66d0997db6306a0f7188
Phil <phillippelliott@gmail.com>
2023-11-09 09:40:47 +0800

    created index to export components in workouts folder

---
+++ components/workouts/index.ts
@@ -0,0 +1,6 @@
+import Activity from "./Activity";
+import Header from "./Header";
+import ScrollCalendar from "./ScrollCalendar";
+import Upcoming from "./Upcoming";
+
+export { Activity, Header, ScrollCalendar, Upcoming };
